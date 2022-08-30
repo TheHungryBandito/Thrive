cff1afae5f71071c101505d7ffe956bad6112293
Oliveriver <oliveriver1@gmail.com>
2022-08-30 16:53:04 +0100

    Add toggle to new game settings

--- src/general/WorldGenerationSettings.cs
+++ src/general/WorldGenerationSettings.cs
@@ -107,6 +107,11 @@ public class WorldGenerationSettings
     public bool EasterEggs { get; set; } = true;
 
     /// <summary>
+    ///   Whether the day/night cycle in this game is enabled
+    /// </summary>
+    public bool DayNightEnabled { get; set; }
+
+    /// <summary>
     ///   The auto-evo configuration this world uses
     /// </summary>
     public IAutoEvoConfiguration AutoEvoConfiguration { get; set; } =
@@ -120,8 +125,9 @@ public class WorldGenerationSettings
             $", Life origin: {Origin}" +
             $", Seed: {Seed}" +
             $", Map type: {MapType}" +
-            $", Include Multicellular: {IncludeMulticellular}" +
+            $", Include multicellular: {IncludeMulticellular}" +
             $", Easter eggs: {EasterEggs}" +
+            $", Day/night cycle: {DayNightEnabled}" +
             "]";
     }
 }
